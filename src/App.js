713928f829d1d7cdd2171c5913370bad3ce40dc3
David Hartunian <david@positiondev.com>
2016-09-12 23:46:40 -0400

    Make it fancier, keyboard + dots

--- src/App.js
+++ src/App.js
@@ -76,21 +76,55 @@ class IdoraStage extends Component {
 class IdoraNav extends Component {
 
   render() {
+    const prevDisabledClass = this.props.disablePrev ? 'idora-disabled' : ''
+    const nextDisabledClass = this.props.disableNext ? 'idora-disabled' : ''
     return (
       <div className='idora-nav'>
-        <div className='idora-prev idora-arrow'
+        <div className={'idora-prev idora-arrow ' + prevDisabledClass}
              onClick={this.props.scrollPrev} />
-        <div className='idora-next idora-arrow'
+        <div className={'idora-next idora-arrow ' + nextDisabledClass}
              onClick={this.props.scrollNext} />
       </div>
     )
   }
 }
 
+class IdoraDot extends Component {
+  render() {
+    const active_class = this.props.active ? 'idora-active' : ''
+    return (
+      <div>
+        <div className={'idora-dot ' + active_class}
+             onClick={this.props.dotClick}>
+        </div>
+      </div>
+    )
+  }
+}
+
 class IdoraDots extends Component {
+  renderDots(carousel_items, scrollTo, leftmost_item, slides_per_dot) {
+    var dots = []
+    carousel_items.forEach(function(item, i) {
+      if (i % slides_per_dot === 0) {
+        const active = (leftmost_item >= i) &&
+                       (leftmost_item < (i + slides_per_dot))
+        dots.push(<IdoraDot active={active}
+                            dotClick={() => scrollTo(i)}
+                            key={i}/>)
+      }
+    })
+    return dots
+  }
+
   render() {
     return (
-      <div></div>
+      <div className='idora-dots'>
+        {this.renderDots(this.props.carousel_items,
+                         this.props.scrollTo,
+                         this.props.leftmost_item,
+                         this.props.slides_per_dot)}
+      </div>
     )
   }
 }
@@ -103,21 +137,28 @@ class Idora extends Component {
       carousel_items: props.carousel_items,
       loop: false,
       leftmost_item: 0,
-      peek: 30
+      peek: 30,
+      slides_per_dot: this.getSlidesPerDotBasedOnWindowWidth()
     }
 
     this.scrollNext = this.scrollNext.bind(this);
     this.scrollPrev = this.scrollPrev.bind(this);
     this.scrollBy = this.scrollBy.bind(this);
+    this.scrollTo = this.scrollTo.bind(this);
+    this.handleResize = this.handleResize.bind(this);
+    this.handleKeyDown = this.handleKeyDown.bind(this);
   }
 
   scrollBy(amount) {
     const leftmost_item = this.state.leftmost_item
     const num_carousel_items = this.state.carousel_items.length
     const new_leftmost = (leftmost_item + amount) % num_carousel_items
+    this.scrollTo(new_leftmost)
+  }
 
+  scrollTo(new_leftmost) {
     this.setState({
-      leftmost_item: new_leftmost
+      leftmost_item: Math.max(0, new_leftmost)
     })
   }
 
@@ -129,6 +170,43 @@ class Idora extends Component {
     this.scrollBy(-1);
   }
 
+  getSlidesPerDotBasedOnWindowWidth() {
+    const w = window.innerWidth;
+    var slides_per_dot = 3;
+
+    if (w > 1500) { slides_per_dot = 1}
+    else if (w > 1100) { slides_per_dot = 2 }
+
+    return slides_per_dot
+  }
+
+  handleResize() {
+    this.setState({
+      slides_per_dot: this.getSlidesPerDotBasedOnWindowWidth()
+    });
+  }
+
+  handleKeyDown(e) {
+    if (e.which === 37) {
+      e.preventDefault();
+      this.scrollPrev();
+    }
+    if (e.which === 39) {
+      e.preventDefault();
+      this.scrollNext();
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
   render() {
     return (
       <div id='idora'>
@@ -138,8 +216,12 @@ class Idora extends Component {
                       peek={this.state.peek}/>
         </div>
         <IdoraNav scrollNext={this.scrollNext} scrollPrev={this.scrollPrev}
-                  showNext={true} showPrev={true} />
-        <IdoraDots />
+                  disableNext={this.state.leftmost_item === this.state.carousel_items.length - 1}
+                  disablePrev={this.state.leftmost_item === 0} />
+        <IdoraDots scrollTo={this.scrollTo}
+                   carousel_items={this.state.carousel_items}
+                   leftmost_item={this.state.leftmost_item}
+                   slides_per_dot={this.state.slides_per_dot}/>
       </div>
     )
   }
